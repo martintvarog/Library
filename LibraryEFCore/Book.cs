b725e1f3ca5f34d95402a96e2179842d991987d7
Tvarog <m_tvarog@utb.cz>
2022-11-11 20:55:28 +0100

    init

---
+++ LibraryEFCore/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryEFCore.Db;
+
+public record Book
+{
+    public string Name { get; init; } = string.Empty;
+
+    public string Author { get; init; } = string.Empty;
+}
---
+++ LibraryEFCore/Db/BooksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryEFCore.Db;
+
+public class BooksDbContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().Property(b => b.Name).IsRequired();
+        modelBuilder.Entity<Book>().Property(b => b.Author).IsRequired();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.Use("Server=(localdb)\\mssqllocaldb;Database=LibraryEFCore;Trusted_Connection=True;");
+    }
+}
