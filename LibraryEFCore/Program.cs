e0d316cf1972f14b3e32209398e23d17c6c631b4
Tvarog <m_tvarog@utb.cz>
2022-11-11 21:44:59 +0100

    refactor

--- LibraryEFCore/Program.cs
+++ LibraryEFCore/Program.cs
@@ -1,22 +1,6 @@
 ﻿using LibraryEFCore.Db;
-using LibraryEFCore.Db.Entity;
 
 using var context = new BooksDbContext();
-var author = new Author
-{
-    FirstName = "John",
-    LastName = "Doe",
-    Books = new List<Book>
-    {
-        new()
-        {
-            Title = "Book 1",
-        },
-        new()
-        {
-            Title = "Book 2",
-        }
-    }
-};
-context.Add(author);
-context.SaveChanges();
+var authors = context.Books.Select(x => x.Author).ToList();
+authors.ForEach(a => Console.WriteLine(a.FirstName + " " + a.LastName));
+Console.ReadKey();
